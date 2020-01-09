6da77fd59442641eafc97775458fd555499cd343
DangThanhTrung1999 <pumaover@gmail.com>
2020-01-09 19:07:47 +0700

    Updates

---
+++ precache-manifest.ff5e4dec3d6b7dd1aa2e2cc93360cf6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b945f423295c4143822f5a9aef394ab",
+    "url": "/CallApiReact/index.html"
+  },
+  {
+    "revision": "948d8ad015d79492e731",
+    "url": "/CallApiReact/static/css/main.f8341c9f.chunk.css"
+  },
+  {
+    "revision": "dbbdfdd303622de61d98",
+    "url": "/CallApiReact/static/js/2.e81d7a0e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/CallApiReact/static/js/2.e81d7a0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "948d8ad015d79492e731",
+    "url": "/CallApiReact/static/js/main.47dda533.chunk.js"
+  },
+  {
+    "revision": "84440a83242aa1359827",
+    "url": "/CallApiReact/static/js/runtime-main.1d35c712.js"
+  }
+]);
