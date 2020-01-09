1bb0360c7af3a2a7d797b25cca258eecafaf3d2f
DangThanhTrung1999 <pumaover@gmail.com>
2020-01-09 13:01:10 +0700

    first commit

---
+++ src/reducers/products.js
@@ -0,0 +1,33 @@
+let initialState = [
+    {
+        id:1,
+        name:'Iphone 6 plus',
+        price:400,
+        status:true
+    },
+    {
+        id:2,
+        name:'Samsung galaxy S&',
+        price:7000,
+        status:true
+    },
+    {
+        id:3,
+        name:'Xiaomi redmi note 8',
+        price:900,
+        status:false
+    },
+    {
+        id:4,
+        name:'Iphone XS plus',
+        price:1200,
+        status:true
+    },
+];
+const products = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return [...state];
+    }
+};
+export default products;
